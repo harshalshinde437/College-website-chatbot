c6cff4b33e3911361ed107af10016992e979047f
harshalshinde437 <harshalshinde437@gmail.com>
2021-06-20 12:39:40 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request #pip install flask
+from chatterbot import ChatBot                  #pip install chatterbot==1.0.1
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app=Flask(__name__)
+#variable name = english_bot, object name = ChatBot, 
+#username = Chatterbot, database = storage_adapter
+#data will be stored in chatterbot.storage.SQLStorageAdapter
+english_bot = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter") 
+
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
+trainer.train("D:\\major project (TYMCA)\\data\\mca_data.yml") #data will store here
+
+@app.route("/")
+def index():
+    return render_template("index.html") #send context to html
+
+@app.route("/get")
+def get_bot_response():
+    userText=request.args.get("msg") #get data from input, we write js to index.html
+    return str(english_bot.get_response(userText))
+
+if __name__=="__main__":
+    app.run(debug=True)
